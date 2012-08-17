a6d1370322889e967117cc760c3229303763cc6d
mwinter <moritz.a.winter@googlemail.com>
2012-08-17 05:01:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SporkError::Application.config.secret_token = 'e3e84a88fda3ca158a0c2f9d7057a915db2160cf7f6e49b6130fffba13d434e37f9e2238ee7aff033d4efa48f6ec051cd71ff6a52a7db97db0638f9717fbbd7b'
